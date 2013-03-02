4c989a2e141e56e3962f6aab2ae180c282eec591
Zach Taylor <zach.taylor673@gmail.com>
2013-03-02 14:05:39 -0600

    Added src and test directories and added JUnit3 to build path

---
+++ src/org/teamrocket/StateEntity.java
@@ -0,0 +1,5 @@
+package org.teamrocket;
+
+public class StateEntity {
+
+}
---
+++ test/org/teamrocket/TestStateEntity.java
@@ -0,0 +1,7 @@
+package org.teamrocket;
+
+import junit.framework.TestCase;
+
+public class TestStateEntity extends TestCase {
+  
+}
