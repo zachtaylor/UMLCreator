768547d59aaea6238e0a7566572a42ccf8e217f1
Kaoutar Maghfour <kaoutar@uwm.edu>
2013-03-02 04:17:32 -0800

    Implemented TransitionEntity equals() method

--- src/org/teamrocket/TransitionEntity.java
+++ src/org/teamrocket/TransitionEntity.java
@@ -15,7 +15,6 @@ public class TransitionEntity {
 
   public StateEntity getPrev() {
     return _prev;
-
   }
 
   public void setInput(String s) {
@@ -32,6 +31,27 @@ public class TransitionEntity {
 
   public String getAction() {
     return _action;
+  }
+
+  public boolean equals(Object o) {
+    if (!(o instanceof TransitionEntity))
+      return false;
+
+    TransitionEntity s = (TransitionEntity) o;
+
+    if ((_input != null && s._input == null) || (_input == null && s._input != null))
+      return false;
+    if (_input != null && s._input != null && !_input.equals(s._input))
+      return false;
+    if ((_action != null && s._action == null) || (_action == null && s._action != null))
+      return false;
+    if (_action != null && !_action.equals(s._action))
+      return false;
+
+    if (!(_next == s.getNext()) || !(_prev == s.getPrev()))
+      return false;
+
+    return true;
   }
 
   private String _input;
