40fb5a52aa109be41bf6f9f095713bcc2d04dc04
Zach Taylor <zach.taylor673@gmail.com>
2013-03-04 19:38:50 -0600

    formatting moar

--- test/org/teamrocket/TestTransitionEntity.java
+++ test/org/teamrocket/TestTransitionEntity.java
@@ -1,18 +1,14 @@
 package org.teamrocket;
 
-import org.junit.Test;
-
 import junit.framework.TestCase;
 
 public class TestTransitionEntity extends TestCase {
-
   TransitionEntity ent;
 
   public void setUp() {
     ent = new TransitionEntity();
   }
 
-  @Test
   public void testNext() {
     StateEntity nextValue = new StateEntity(false);
     ent.setNext(nextValue);
@@ -20,7 +16,6 @@ public class TestTransitionEntity extends TestCase {
     assertEquals(nextValue, ent.getNext());
   }
 
-  @Test
   public void testPrev() {
     StateEntity prevValue = new StateEntity(false);
     ent.setPrev(prevValue);
@@ -28,7 +23,6 @@ public class TestTransitionEntity extends TestCase {
     assertEquals(prevValue, ent.getPrev());
   }
 
-  @Test
   public void testInput() {
     String InputValue = "Fu-bar";
     ent.setInput(InputValue);
@@ -36,7 +30,6 @@ public class TestTransitionEntity extends TestCase {
     assertEquals(InputValue, ent.getInput());
   }
 
-  @Test
   public void testAction() {
     String actionValue = "This transition does this";
     ent.setAction(actionValue);
