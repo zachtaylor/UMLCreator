878512bf1db44df33eee3f76d42f8fa55fd2144c
Zach Taylor <zach.taylor673@gmail.com>
2013-03-09 13:14:55 -0600

    added a main and applicationmodel

---
+++ src/org/teamrocket/Main.java
@@ -0,0 +1,29 @@
+package org.teamrocket;
+
+import org.jhotdraw.app.Application;
+import org.jhotdraw.app.OSXApplication;
+import org.jhotdraw.app.SDIApplication;
+
+public class Main {
+  public static void main(String[] args) {
+    Application app;
+    String os = System.getProperty("os.name").toLowerCase();
+    if (os.startsWith("mac")) {
+      app = new OSXApplication();
+    }
+    else if (os.startsWith("win")) {
+      // app = new DefaultMDIApplication();
+      app = new SDIApplication();
+    }
+    else {
+      app = new SDIApplication();
+    }
+
+    ApplicationModel model = new ApplicationModel();
+    model.setName("JHotDraw Pert");
+    model.setVersion(Main.class.getPackage().getImplementationVersion());
+    model.setViewClassName("org.jhotdraw.samples.pert.PertView");
+    app.setModel(model);
+    app.launch(args);
+  }
+}
