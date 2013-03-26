2774c00befd824d2361aa63d631c466497006dc1
Sam Dembinski <dembins3@uwm.edu>
2013-03-25 23:11:34 -0500

    zach insists on a push
    started to add handles to startstatefigure, but it's not all there yet

--- src/org/teamrocket/StartStateFigure.java
+++ src/org/teamrocket/StartStateFigure.java
@@ -1,8 +1,16 @@
 package org.teamrocket;
 
 import java.awt.geom.Point2D;
+import java.util.Collection;
+import java.util.LinkedList;
 
 import org.jhotdraw.draw.EllipseFigure;
+import org.jhotdraw.draw.connector.LocatorConnector;
+import org.jhotdraw.draw.handle.BoundsOutlineHandle;
+import org.jhotdraw.draw.handle.ConnectorHandle;
+import org.jhotdraw.draw.handle.Handle;
+import org.jhotdraw.draw.handle.MoveHandle;
+import org.jhotdraw.draw.locator.RelativeLocator;
 import org.jhotdraw.util.ResourceBundleUtil;
 
 public class StartStateFigure extends EllipseFigure {
@@ -18,7 +26,29 @@ public class StartStateFigure extends EllipseFigure {
 		
   }
 
- 
+  @Override
+	public Collection<Handle> createHandles(int detailLevel) {
+		java.util.List<Handle> handles = new LinkedList<Handle>();
+
+		switch (detailLevel) {
+		case -1:
+			handles
+					.add(new BoundsOutlineHandle(this, false, true));
+			break;
+		case 0:
+			 handles.add(new MoveHandle(this, RelativeLocator.northWest()));
+			 handles.add(new MoveHandle(this, RelativeLocator.northEast()));
+			 handles.add(new MoveHandle(this, RelativeLocator.southWest()));
+			 handles.add(new MoveHandle(this, RelativeLocator.southEast()));
+			 
+			ConnectorHandle ch;
+			handles.add(ch = new ConnectorHandle(new LocatorConnector(this,
+					RelativeLocator.center()), new TransitionFigure()));
+			ch.setToolTipText("Drag the connector to a dependent state.");
+			break;
+		}
+		return handles;
+	}
   
   
   @Override
