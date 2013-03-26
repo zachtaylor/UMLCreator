3a4312e2315e1a0d6597b4a430089c656af6847c
Sam Dembinski <dembins3@uwm.edu>
2013-03-26 00:15:44 -0500

    endStateFigures now have connect handles

--- src/org/teamrocket/EndStateFigure.java
+++ src/org/teamrocket/EndStateFigure.java
@@ -3,9 +3,17 @@ package org.teamrocket;
 import java.awt.Color;
 import java.awt.Graphics2D;
 import java.awt.geom.Point2D;
+import java.util.Collection;
+import java.util.LinkedList;
 
 import org.jhotdraw.draw.AttributeKeys;
 import org.jhotdraw.draw.EllipseFigure;
+import org.jhotdraw.draw.connector.LocatorConnector;
+import org.jhotdraw.draw.handle.BoundsOutlineHandle;
+import org.jhotdraw.draw.handle.ConnectorHandle;
+import org.jhotdraw.draw.handle.Handle;
+import org.jhotdraw.draw.handle.MoveHandle;
+import org.jhotdraw.draw.locator.RelativeLocator;
 
 public class EndStateFigure extends EllipseFigure {
   public EllipseFigure circle;
@@ -17,6 +25,30 @@ public class EndStateFigure extends EllipseFigure {
 
   }
 
+  @Override
+	public Collection<Handle> createHandles(int detailLevel) {
+		java.util.List<Handle> handles = new LinkedList<Handle>();
+
+		switch (detailLevel) {
+		case -1:
+			handles
+					.add(new BoundsOutlineHandle(this, false, true));
+			break;
+		case 0:
+			 handles.add(new MoveHandle(this, RelativeLocator.northWest()));
+			 handles.add(new MoveHandle(this, RelativeLocator.northEast()));
+			 handles.add(new MoveHandle(this, RelativeLocator.southWest()));
+			 handles.add(new MoveHandle(this, RelativeLocator.southEast()));
+			 
+			ConnectorHandle ch;
+			handles.add(ch = new ConnectorHandle(new LocatorConnector(this,
+					RelativeLocator.center()), new TransitionFigure()));
+			ch.setToolTipText("Drag the connector to a dependent state.");
+			break;
+		}
+		return handles;
+	}
+  
   public void draw(Graphics2D g) {
     set(AttributeKeys.FILL_COLOR, Color.BLACK);
     super.draw(g);
