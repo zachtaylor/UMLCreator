17ad85b199e5e2c116639b3949c97f91a4bd65bb
Kaoutar Maghfour <kaoutar@uwm.edu>
2013-04-28 05:47:32 -0700

    testing something

--- lib/JHotDraw 7.6/Source/jhotdraw7/src/main/java/org/jhotdraw/draw/AbstractDrawing.java
+++ lib/JHotDraw 7.6/Source/jhotdraw7/src/main/java/org/jhotdraw/draw/AbstractDrawing.java
@@ -95,6 +95,7 @@ public abstract class AbstractDrawing extends AbstractAttributedCompositeFigure 
     public void write(DOMOutput out) throws IOException {
         out.openElement("figures");
         for (Figure f : getChildren()) {
+          System.out.println(f);
             out.writeObject(f);
         }
         out.closeElement();
